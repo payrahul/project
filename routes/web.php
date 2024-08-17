2e55dc4582e8648a2465ccacf78c47d3a564f12e
rahul verma <rahulpay006@gmail.com>
2024-08-17 10:08:59 +0530

    razor pay for test or first

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,15 @@
 <?php
 
+use App\Http\Controllers\RazorpayController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TodoController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('todo', TodoController::class);
-Route::post('/save-task', [TodoController::class, 'create']);
-Route::get('/get-tasks', [TodoController::class, 'geTasks']);
-Route::post('/delete-task', [TodoController::class, 'destroy']);
-Route::post('/complete-task', [TodoController::class, 'completeTask']);
-Route::post('/show-alltask', [TodoController::class, 'showAlltask']);
-
+Route::get('payment',[RazorpayController::class,'index']);
+Route::post('payment/create',[RazorpayController::class,'store'])->name('razorpay.payment.store');
+Route::post('payment/failure',[RazorpayController::class,'failure'])->name('razorpay.payment.failure');
+Route::get('/402', function () {
+    return view('errors.402');
+});
